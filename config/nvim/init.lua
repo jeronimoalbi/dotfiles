8a0a89681c8f9568658c045d08278673a13b628f
Jerónimo Albi <jeronimo.albi@gmail.com>
2021-12-09 19:34:25 +0100

    Rewrite vim init to Lua

---
+++ config/nvim/init.lua
@@ -0,0 +1,57 @@
+vim.g.python_host_prog = "python"
+vim.g.python3_host_prog = "python3"
+
+-- Use comma as shortcut prefix
+vim.g.mapleader = ","
+
+-- Allow vim to switch buffers when when current buffer is not saved
+vim.opt.hidden = true
+
+-- Turn backup files off
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.swapfile = false
+
+-- Make sure 4 spaces are used instead of tab
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- Enables 24-bit RGB color
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+
+-- Install plugins
+require('plugins')
+
+-- Select the color theme to enable.
+-- NOTE: Plugins must be loaded before.
+vim.cmd [[ colorscheme seoul256 ]]
+
+-- Enable selected line marker after themes was applied
+vim.opt.cursorline = true
+vim.cmd [[
+    autocmd WinEnter * setlocal cursorline
+    autocmd WinLeave * setlocal nocursorline
+]]
+
+-- By default load tags from 'tags' files
+vim.opt.tags = {'tags'}
+
+-- Setup plugins
+vim.cmd [[ runtime custom/plugins_setup.vim ]]
+-- Custom commands
+vim.cmd [[ runtime custom/commands.vim ]]
+
+-- Load settings by file type
+vim.cmd [[
+    au BufRead,BufNewFile *.py runtime custom/autoload/python.vim
+    au BufRead,BufNewFile *.go runtime custom/autoload/go.vim
+    au BufRead,BufNewFile *.json runtime custom/autoload/json.vim
+    au BufRead,BufNewFile *.js runtime custom/autoload/javascript.vim
+    au BufRead,BufNewFile *.jsx runtime custom/autoload/javascript.vim
+    au BufRead,BufNewFile *.sol runtime custom/autoload/solidity.vim
+    au BufRead,BufNewFile *.xml runtime custom/autoload/html.vim
+    au BufRead,BufNewFile *.html runtime custom/autoload/html.vim
+]]
---
+++ config/nvim/lua/plugins.lua
@@ -0,0 +1,106 @@
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin', '~/.config/nvim/plugged')
+
+-- == Fonts ==
+--
+-- Requires manual font installation
+--
+Plug 'tonsky/firacode'
+
+-- == Themes ==
+--
+Plug 'junegunn/seoul256.vim'
+Plug 'w0ng/vim-hybrid'
+Plug 'freeo/vim-kalisi'
+Plug 'mhartington/oceanic-next'
+Plug 'altercation/vim-colors-solarized'
+Plug 'Lokaltog/vim-distinguished'
+Plug 'jonathanfilip/vim-lucius'
+Plug 'tomasr/molokai'
+Plug 'arcticicestudio/nord-vim'
+
+-- == Common ==
+--
+Plug 'scrooloose/nerdtree'
+Plug 'vim-airline/vim-airline'
+Plug 'vim-airline/vim-airline-themes'
+Plug 'simnalamburt/vim-mundo'
+Plug 'tpope/vim-repeat'
+Plug 'matze/vim-move'
+Plug 'dhruvasagar/vim-zoom'
+Plug 'majutsushi/tagbar'
+Plug 'Yggdroot/indentLine'
+Plug 'bitc/vim-bad-whitespace'
+Plug 'tomtom/tcomment_vim'
+Plug 'briandoll/change-inside-surroundings.vim'
+Plug 'nvim-lua/plenary.nvim'
+
+-- == LSP ==
+--
+-- CocList extensions -> Manage installed extensions
+-- CocUpdate -> Update extensions
+--
+-- Go:
+--   CocInstall coc-go
+-- Python:
+--   pip install -U jedi-language-server
+--   CocInstall coc-jedi
+-- Emmet:
+--   CocInstall coc-emmet
+-- Javascript:
+--   CocInstall coc-json coc-tsserver coc-eslint coc-prettier coc-css
+--   CocInstall coc-flow
+--   CocInstall coc-graphql
+--   CocInstall coc-tailwindcss
+-- YAML:
+--   CocInstall coc-yaml
+-- Swagger:
+--   CocInstall coc-swagger
+-- SQL:
+--   CocInstall coc-sql
+--
+Plug 'neovim/nvim-lspconfig'
+Plug('neoclide/coc.nvim', {branch = 'release'})
+
+-- == Syntax ==
+--
+Plug 'yuezk/vim-js'
+Plug 'HerringtonDarkholme/yats.vim'
+Plug 'maxmellon/vim-jsx-pretty'
+Plug 'tomlion/vim-solidity'
+
+-- == Development ==
+--
+Plug 'ctrlpvim/ctrlp.vim'
+Plug 'sgur/ctrlp-extensions.vim'
+Plug 'ivalkeen/vim-ctrlp-tjump'
+Plug 'lokikl/vim-ctrlp-ag'
+Plug 'tpope/vim-fugitive'
+Plug 'mhinz/vim-signify'
+Plug 'editorconfig/editorconfig-vim'
+Plug('fatih/vim-go', {['do'] = ':GoInstallBinaries'})
+Plug('prettier/vim-prettier', {['do'] = 'npm install', ['for'] = {'javascript', 'solidity'}})
+Plug 'dmdque/solidity.vim'
+
+-- == AnyJump ==
+--    macos -> brew install ripgrep
+--    linux -> sudo apt-get install ripgrep
+--
+Plug 'pechorin/any-jump.vim'
+
+-- == MacOS ==
+--
+if vim.fn.system('uname -s') == "Darwin\n" then
+  Plug 'rizzatti/dash.vim'
+end
+
+-- == Graveyard ==
+--
+-- Plug 'benekastah/neomake'
+-- Plug 'sbdchd/neoformat'
+-- Plug 'mattn/emmet-vim'
+-- Plug('stsewd/isort.nvim', {['do'] = ':UpdateRemotePlugins'})
+-- Plug('psf/black', {branch = 'stable'})
+
+vim.call('plug#end')
