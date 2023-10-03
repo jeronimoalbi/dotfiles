dd5983c4a8be700a99bf926545684b4320b77ad5
jeronimoalbi <jeronimo.albi@gmail.com>
2023-10-03 19:25:35 +0200

    feat: add Telescope keymaps

--- config/nvim/lua/core/keymaps.lua
+++ config/nvim/lua/core/keymaps.lua
@@ -1,1 +1,2 @@
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>', { silent = true })
--- config/nvim/lua/core/plugins_config/telescope.lua
+++ config/nvim/lua/core/plugins_config/telescope.lua
@@ -2,7 +2,47 @@ require("telescope").setup()
 require("telescope").load_extension("fzf")
 require("telescope").load_extension("undo")
 
-vim.keymap.set("n", "<leader>u", ":Telescope undo<CR>", { silent = true })
+local builtin = require("telescope.builtin")
 
--- local builtin = require("telescope.builtin")
--- vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set("n", "<leader>b", builtin.buffers, {
+  desc = "List Buffers",
+  noremap = true,
+  silent = true,
+})
+vim.keymap.set("n", "<leader>/", builtin.current_buffer_fuzzy_find, {
+  desc = "Fuzzy Find Buffer",
+  noremap = true,
+  silent = true,
+})
+vim.keymap.set("n", "<leader>d", builtin.oldfiles, {
+  desc = "Find Files (History)",
+  noremap = true,
+  silent = true,
+})
+vim.keymap.set("n", "<leader>f", builtin.find_files, {
+  desc = "Find Files",
+  noremap = true,
+  silent = true,
+})
+vim.keymap.set("n", "<leader>g", builtin.git_files, {
+  desc = "Find Files (Git)",
+  noremap = true,
+  silent = true,
+})
+vim.keymap.set("n", "<leader>f", builtin.live_grep, {
+  desc = "Live Grep",
+  noremap = true,
+  silent = true,
+})
+vim.keymap.set("n", "<leader>s", builtin.lsp_dynamic_workspace_symbols, {
+  desc = "Search Workspace Symbols",
+  noremap = true,
+  silent = true,
+})
+vim.keymap.set("n", "<leader>x", builtin.lsp_document_symbols, {
+  desc = "List Document Symbols",
+  noremap = true,
+  silent = true,
+})
+
+vim.keymap.set("n", "<leader>u", ":Telescope undo<CR>", { silent = true })
