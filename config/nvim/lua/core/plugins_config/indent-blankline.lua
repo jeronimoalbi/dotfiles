df7575f84509eb3badbc06746104723f58d2f3db
jeronimoalbi <jeronimo.albi@gmail.com>
2023-10-04 15:43:22 +0200

    feat: disable the first indentation line in vim

--- config/nvim/lua/core/plugins_config/indent-blankline.lua
+++ config/nvim/lua/core/plugins_config/indent-blankline.lua
@@ -1,9 +1,18 @@
--- Change indent-blankline.nvim to avoid 1st level indent guide
-vim.g.indent_blankline_show_first_indent_level = false
-
 require("ibl").setup({
   scope = {
     show_start = false,
     show_end = false,
   },
 })
+
+local hooks = require("ibl.hooks")
+
+-- Change indent-blankline.nvim to avoid 1st level indent guide
+hooks.register(
+  hooks.type.WHITESPACE,
+  hooks.builtin.hide_first_space_indent_level
+)
+hooks.register(
+  hooks.type.WHITESPACE,
+  hooks.builtin.hide_first_tab_indent_level
+)
