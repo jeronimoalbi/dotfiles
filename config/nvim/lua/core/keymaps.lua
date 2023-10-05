d35afe9fed52310b0ad9a7ce6f2e6fb4ca03b5f5
jeronimoalbi <jeronimo.albi@gmail.com>
2023-10-05 17:44:00 +0200

    feat: add diagnostics popup support to vim

--- config/nvim/lua/core/keymaps.lua
+++ config/nvim/lua/core/keymaps.lua
@@ -1,2 +1,3 @@
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>', { silent = true })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
