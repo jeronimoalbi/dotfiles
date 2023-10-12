4e9f667d407bd63305a8e0d37432c2f721b58c3c
Jerónimo Albi <jeronimo.albi@gmail.com>
2023-10-12 19:51:59 +0200

    feat: change vim LSP config to use clippy for rust checks

--- config/nvim/lua/core/plugins_config/lspconfig.lua
+++ config/nvim/lua/core/plugins_config/lspconfig.lua
@@ -89,6 +89,13 @@ rust_tools.setup({
   server = {
     on_attach = on_attach,
     capabilities = capabilities,
+    settings = {
+      ["rust-analyzer"] = {
+        checkOnSave = {
+          command = "clippy",
+        },
+      },
+    },
     -- on_attach = function(_, bufnr)
     --   -- Hover actions
     --   vim.keymap.set("n", "<C-space>", rust_tools.hover_actions.hover_actions, { buffer = bufnr })
