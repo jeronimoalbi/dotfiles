7263d843e251abed0de0d22c72f9a52ae0c34ca7
jeronimoalbi <894299+jeronimoalbi@users.noreply.github.com>
2026-07-11 20:31:32 +0200

    fix: change neovim to properly auto format Rust code

--- config/nvim/lua/core/plugins_config/conform.lua
+++ config/nvim/lua/core/plugins_config/conform.lua
@@ -8,20 +8,6 @@ require("conform").setup({
     -- css  = { "prettier" },
     -- scss = { "prettier" },
   },
-  formatters = {
-    rustfmt = {
-      prepend_args = function(_, ctx)
-        local cargo = ctx.dirname .. "/Cargo.toml"
-        if vim.fn.filereadable(cargo) == 1 then
-          for _, line in ipairs(vim.fn.readfile(cargo)) do
-            local edition = line:match('^edition%s*=%s*"(%d+)"')
-            if edition then return { "--edition=" .. edition } end
-          end
-        end
-        return { "--edition=2021" }
-      end,
-    },
-  },
   format_on_save = {
     timeout_ms = 7000,
     lsp_fallback = false,
